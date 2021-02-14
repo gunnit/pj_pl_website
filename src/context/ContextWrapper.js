95bd0c61f1ec5c0577503c73ad55a92c23519110
Mark Mansolino <markjm610@gmail.com>
2021-02-14 16:55:13 -0500

    Context

--- src/context/ContextWrapper.js
+++ src/context/ContextWrapper.js
@@ -1,11 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Context from './Context';
 
 export default function ContextWrapper({ children }) {
-    return (
-        <Context.Provider value={{
 
-        }}>
+    return (
+        <Context.Provider value={{}}>
             {children}
         </Context.Provider>
     )
