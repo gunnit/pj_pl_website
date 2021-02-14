31231fd3457884578c393332eee064ab5bc65d9e
Mark Mansolino <markjm610@gmail.com>
2021-02-14 03:20:20 -0500

    Add images to taxonomy page

--- src/views/custom/discovery/TaxonomyView/index.js
+++ src/views/custom/discovery/TaxonomyView/index.js
@@ -24,52 +24,52 @@ function TaxonomyView() {
                 </Box>
                 <Grid container spacing={3}>
                     <Grid item xs={12} sm={6} md={4} lg={3}>
-                        <GenericBoxInfo description={'Develop Vision and Strategy'} imagePath={'/static/icons/taxonomy/undraw_mindfulness_scgo.svg'} clickPath={PATH_WIKI.developVisionAndStrategy} />
+                        <GenericBoxInfo description={'Develop Vision and Strategy'} imagePath={'/static/images/taxonomy/undraw_mindfulness_scgo.svg'} clickPath={PATH_WIKI.developVisionAndStrategy} />
                     </Grid>
                     <Grid item xs={12} sm={6} md={4} lg={3}>
-                        <GenericBoxInfo description={'Develop and Manage Products and Services'} imagePath={'/static/icons/taxonomy/undraw_product_teardown_elol.svg'} clickPath={PATH_WIKI.developManageProductsServices} />
+                        <GenericBoxInfo description={'Develop and Manage Products and Services'} imagePath={'/static/images/taxonomy/undraw_product_teardown_elol.svg'} clickPath={PATH_WIKI.developManageProductsServices} />
                     </Grid>
                     <Grid item xs={12} sm={6} md={4} lg={3}>
-                        <GenericBoxInfo description={'Market and Sell Products'} imagePath={'/static/icons/taxonomy/undraw_Marketing_re_7f1g.svg'} clickPath={PATH_WIKI.marketSellProducts} />
+                        <GenericBoxInfo description={'Market and Sell Products'} imagePath={'/static/images/taxonomy/undraw_Marketing_re_7f1g.svg'} clickPath={PATH_WIKI.marketSellProducts} />
                     </Grid>
                     <Grid item xs={12} sm={6} md={4} lg={3}>
-                        <GenericBoxInfo description={'Deliver Physical Products'} imagePath={'/static/icons/taxonomy/undraw_On_the_way_re_swjt.svg'} clickPath={PATH_WIKI.deliverPhysicalProducts} />
+                        <GenericBoxInfo description={'Deliver Physical Products'} imagePath={'/static/images/taxonomy/undraw_On_the_way_re_swjt.svg'} clickPath={PATH_WIKI.deliverPhysicalProducts} />
                     </Grid>
                     <Grid item xs={12} sm={6} md={4} lg={3}>
-                        <GenericBoxInfo description={'Deliver Services'} imagePath={'/static/icons/taxonomy/undraw_tutorial_video_vtd1.svg'} clickPath={PATH_WIKI.deliverServices} />
+                        <GenericBoxInfo description={'Deliver Services'} imagePath={'/static/images/taxonomy/undraw_tutorial_video_vtd1.svg'} clickPath={PATH_WIKI.deliverServices} />
                     </Grid>
                     <Grid item xs={12} sm={6} md={4} lg={3}>
-                        <GenericBoxInfo description={'Manage Customer Service'} imagePath={'/static/icons/taxonomy/undraw_reviews_lp8w.svg'} clickPath={PATH_WIKI.manageCustomerService} />
+                        <GenericBoxInfo description={'Manage Customer Service'} imagePath={'/static/images/taxonomy/undraw_reviews_lp8w.svg'} clickPath={PATH_WIKI.manageCustomerService} />
                     </Grid>
                     <Grid item xs={12} sm={6} md={4} lg={3}>
-                        <GenericBoxInfo description={'Develop and Manage Human Capital'} imagePath={'/static/icons/taxonomy/undraw_team_spirit_hrr4.svg'} clickPath={PATH_WIKI.developManageHumanCapital} />
+                        <GenericBoxInfo description={'Develop and Manage Human Capital'} imagePath={'/static/images/taxonomy/undraw_team_spirit_hrr4.svg'} clickPath={PATH_WIKI.developManageHumanCapital} />
                     </Grid>
                     <Grid item xs={12} sm={6} md={4} lg={3}>
-                        <GenericBoxInfo description={'Manage Information Technology (IT)'} imagePath={'/static/icons/taxonomy/undraw_programming_2svr.svg'} clickPath={PATH_WIKI.manageInformationTechnology} />
+                        <GenericBoxInfo description={'Manage Information Technology (IT)'} imagePath={'/static/images/taxonomy/undraw_programming_2svr.svg'} clickPath={PATH_WIKI.manageInformationTechnology} />
                     </Grid>
                     <Grid item xs={12} sm={6} md={4} lg={3}>
-                        <GenericBoxInfo description={'Manage Financial Resources'} imagePath={'/static/icons/taxonomy/undraw_predictive_analytics_kf9n.svg'} clickPath={PATH_WIKI.manageFinancialResources} />
+                        <GenericBoxInfo description={'Manage Financial Resources'} imagePath={'/static/images/taxonomy/undraw_predictive_analytics_kf9n.svg'} clickPath={PATH_WIKI.manageFinancialResources} />
                     </Grid>
                     <Grid item xs={12} sm={6} md={4} lg={3}>
-                        <GenericBoxInfo description={'Acquire, Construct, and Manage Assets'} imagePath={'/static/icons/taxonomy/undraw_add_to_cart_vkjp.svg'} clickPath={PATH_WIKI.acquireConstructManageAssets} />
+                        <GenericBoxInfo description={'Acquire, Construct, and Manage Assets'} imagePath={'/static/images/taxonomy/undraw_add_to_cart_vkjp.svg'} clickPath={PATH_WIKI.acquireConstructManageAssets} />
                     </Grid>
                     <Grid item xs={12} sm={6} md={4} lg={3}>
-                        <GenericBoxInfo description={'Manage Enterprise Risk, Compliance, Remediation, and Resiliency'} imagePath={'/static/icons/taxonomy/undraw_warning_cyit.svg'} clickPath={PATH_WIKI.manageEnterpriseRiskCompliance} />
+                        <GenericBoxInfo description={'Manage Enterprise Risk, Compliance, Remediation, and Resiliency'} imagePath={'/static/images/taxonomy/undraw_warning_cyit.svg'} clickPath={PATH_WIKI.manageEnterpriseRiskCompliance} />
                     </Grid>
                     <Grid item xs={12} sm={6} md={4} lg={3}>
-                        <GenericBoxInfo description={'Manage External Relationships'} imagePath={'/static/icons/taxonomy/undraw_word_of_mouth_v1j9.svg'} clickPath={PATH_WIKI.manageExternalRelationships} />
+                        <GenericBoxInfo description={'Manage External Relationships'} imagePath={'/static/images/taxonomy/undraw_word_of_mouth_v1j9.svg'} clickPath={PATH_WIKI.manageExternalRelationships} />
                     </Grid>
                     <Grid item xs={12} sm={6} md={4} lg={3}>
-                        <GenericBoxInfo description={'Develop and Manage Business Capabilities'} imagePath={'/static/icons/taxonomy/undraw_Business_plan_re_0v81.svg'} clickPath={PATH_WIKI.developManageBusinessCapabilities} />
+                        <GenericBoxInfo description={'Develop and Manage Business Capabilities'} imagePath={'/static/images/taxonomy/undraw_Business_plan_re_0v81.svg'} clickPath={PATH_WIKI.developManageBusinessCapabilities} />
                     </Grid>
                     <Grid item xs={12} sm={6} md={4} lg={3}>
-                        <GenericBoxInfo description={'Search all Processes'} imagePath={'/static/icons/taxonomy/undraw_clean_up_ucm0.svg'} clickPath={PATH_WIKI.searchAllProcesses} />
+                        <GenericBoxInfo description={'Search all Processes'} imagePath={'/static/images/taxonomy/undraw_clean_up_ucm0.svg'} clickPath={PATH_WIKI.searchAllProcesses} />
                     </Grid>
                     <Grid item xs={12} sm={6} md={4} lg={3}>
-                        <GenericBoxInfo description={'View per Process Groups'} imagePath={'/static/icons/taxonomy/undraw_Mind_map_re_nlb6.svg'} clickPath={PATH_WIKI.viewProcessGroups} />
+                        <GenericBoxInfo description={'View per Process Groups'} imagePath={'/static/images/taxonomy/undraw_Mind_map_re_nlb6.svg'} clickPath={PATH_WIKI.viewProcessGroups} />
                     </Grid>
                     <Grid item xs={12} sm={6} md={4} lg={3}>
-                        <GenericBoxInfo description={'View only Processes'} imagePath={'/static/icons/taxonomy/undraw_career_development_oqcb.svg'} clickPath={PATH_WIKI.viewOnlyProcesses} />
+                        <GenericBoxInfo description={'View only Processes'} imagePath={'/static/images/taxonomy/undraw_career_development_oqcb.svg'} clickPath={PATH_WIKI.viewOnlyProcesses} />
                     </Grid>
                 </Grid>
             </Container>
