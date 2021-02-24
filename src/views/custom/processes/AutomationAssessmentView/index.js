dd75399b6e7f4d2461d933f0b6621e853b4bb2cf
Mark Mansolino <markjm610@gmail.com>
2021-02-23 22:27:16 -0500

    Scores

--- src/views/custom/processes/AutomationAssessmentView/index.js
+++ src/views/custom/processes/AutomationAssessmentView/index.js
@@ -181,6 +181,7 @@ export default function AutomationAssessmentView() {
     const [questions, setQuestions] = useState(null)
     const [formikInitialValues, setFormikInitialValues] = useState({})
     const [formikValues, setFormikValues] = useState({})
+    const [weights, setWeights] = useState({})
     const [subgroups, setSubgroups] = useState(null)
     const [openDialog, setOpenDialog] = useState(false);
 
@@ -241,14 +242,18 @@ export default function AutomationAssessmentView() {
                     storedProcessId = localStorage.getItem('currentProcessId')
                 }
 
-                const res = await fetch(`${apiBaseUrl}/update_details/${currentProcessId || storedProcessId}`, {
-                    method: 'PATCH',
-                    body: JSON.stringify(formikValues),
+                const res = await fetch(`${apiBaseUrl}/automation_assessment/${userId}/${currentProcessId || storedProcessId}`, {
+                    method: 'POST',
+                    body: JSON.stringify({ ...formikValues, weights: { ...weights } }),
                     headers: {
                         "Content-Type": 'application/json',
                         'Authorization': token
                     }
                 })
+
+                if (!res.ok) {
+                    throw res
+                }
 
                 setActiveStep(prevActiveStep => prevActiveStep + 1);
 
@@ -282,65 +287,9 @@ export default function AutomationAssessmentView() {
                     }
                 })
 
-                const { questions, process_details } = await res.json()
+                const { questions } = await res.json()
 
 
-                const initialValues = {
-                    'Multiple Data Sources': process_details.multiple_data_sources,
-                    'Demand Fluctuation': process_details.demand_fluctuation,
-                    'Execution Frequency': process_details.frequency,
-                    'Real Time': process_details.real_time,
-                    'Supervisor Number': process_details.supervisor_number,
-                    'Gathering Footprint': process_details.data_gathering_footprint,
-                    'Time For Execution': process_details.time_for_execution,
-                    'Staff Shuffling Present': process_details.shuffle_staffing,
-
-                    'Historic Data': process_details.historical_data,
-                    'Data Standardization': process_details.data_standardization,
-                    'Incorrect Data': process_details.incorrect_data,
-                    'Reconciliation': process_details.reconciliation,
-                    'Data Rework': process_details.data_rework,
-                    'Number of Applications': process_details.application,
-
-                    'Hand Structured Input': process_details.hand_structured,
-                    'Hand Unstructured Input:': process_details.hand_unstructured,
-                    'Computer Structured Input': process_details.computer_structured,
-                    'Computer Unstructured Input': process_details.computer_unstructured,
-                    'Verbal Communication': process_details.verbal_app,
-                    'Instant Communication': process_details.im_app,
-
-                    'Technology Stability': process_details.tech_stability,
-                    'Future Tech Changes': process_details.tech_changes,
-                    'Security Restrictions': process_details.security_restrictions,
-                    'Citrix App': process_details.citrix_app,
-                    'Mainframe App': process_details.mainframe_app,
-                    'Sap Screen': process_details.sap_screen,
-
-                    // applications_tec , 
-
-                    'Rule Based': process_details.rule_type,
-                    'Transactional': process_details.transactional,
-                    'Process Stability': process_details.process_stability,
-                    'Swivel Activities': process_details.swivel,
-                    'Clear Process Objectives': process_details.process_objectives,
-                    'Stable Rules': process_details.stable_rules,
-                    'Number Of Transactions': process_details.number_transactions,
-                    'External Factors': process_details.external_factors,
-
-                    'Number of Employees': process_details.employee_num,
-                    'Number of FTE': process_details.fte_num,
-                    'Employee Knowledge of Process': process_details.fte_expl,
-                    'Employee Compensation': process_details.salary,
-                    'Pain Point': process_details.pain_points,
-                    'Rework Done': process_details.reworks_done,
-                    'End to End execution': process_details.end_toend,
-                    'Application Error': process_details.app_errors,
-                    'Regulatory Requirements': process_details.reg_req,
-                    'Process Standardization': process_details.process_standardization,
-                    'Document Procedures': process_details.documented_procedures,
-                    'Multiple Teams': process_details.multiple_teams,
-                    'Process Owners': process_details.process_owners,
-                }
 
                 // Store subgroups. This requires subgroups to be in the correct order from the database
 
@@ -362,26 +311,31 @@ export default function AutomationAssessmentView() {
 
                 const questionsSortedBySubgroup = {}
 
-                // const initialValues = {}
+                const initialValues = {}
+
+                const weights = {}
 
                 questions.forEach(question => {
 
-                    // Future version:
-                    // initialValues[question.id] = '' not sure about data type for future version
+                    if (question.score) {
+                        initialValues[question.id] = question.score.answer // this is a number as a string
+                    } else {
+                        initialValues[question.id] = '0'
+                    }
 
-                    // initialValues[question.title] = '0'
+                    weights[question.id] = question.weight
 
                     if (questionsSortedBySubgroup[question.subgroup]) {
                         questionsSortedBySubgroup[question.subgroup].push(question)
                     } else {
                         questionsSortedBySubgroup[question.subgroup] = [question]
-
                     }
+
                 })
                 // console.log(initialValues)
                 setFormikInitialValues(initialValues)
                 setQuestions(questionsSortedBySubgroup)
-
+                setWeights(weights)
 
             })()
         }
@@ -405,8 +359,8 @@ export default function AutomationAssessmentView() {
                         <>
                             {questions[step].map(question => {
                                 return (
-                                    <>
-                                        <Typography key={question.id.toString()} variant="subtitle1" gutterBottom>
+                                    <div key={question.id.toString()}>
+                                        <Typography variant="subtitle1" gutterBottom>
                                             {question.question}
                                         </Typography >
                                         <TextField
@@ -416,12 +370,13 @@ export default function AutomationAssessmentView() {
                                             label={question.title}
                                             onChange={(e) => {
                                                 handleChange(e)
+                                                // Why? I think it had to do with scope and not wanting to rearrange everything
                                                 setFormikValues(values)
+                                                // setAnswerTexts(previous => ({...previous, }))
                                             }}
                                             // Formik uses the name as the key to keep track of the field in the values object like this: values[name]
-                                            // In the future, name and value will be question.id so it is unique but for right now the title is easier to work with
-                                            name={question.title}
-                                            value={values[question.title]}
+                                            name={question.id}
+                                            value={values[question.id]}
                                             className={classes.margin}
                                         >
                                             {question.answers.map((answer, i) => (
@@ -431,7 +386,7 @@ export default function AutomationAssessmentView() {
                                                 </MenuItem>
                                             ))}
                                         </TextField>
-                                    </>
+                                    </div>
                                 )
                             })}
                         </>
