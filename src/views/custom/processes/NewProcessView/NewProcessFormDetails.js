e20d81022c9b6765e541e725904b0c090c034808
Mark Mansolino <markjm610@gmail.com>
2021-03-01 00:17:59 -0500

    new process form

--- src/views/custom/processes/NewProcessView/NewProcessFormDetails.js
+++ src/views/custom/processes/NewProcessView/NewProcessFormDetails.js
@@ -6,6 +6,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import {
   TextField,
   MenuItem,
+  Grid,
 } from '@material-ui/core';
 
 // ----------------------------------------------------------------------
@@ -47,73 +48,84 @@ function NewProcessFormDetails({ formik, className, triedToClickPast, ...other }
         className={clsx(classes.root, className)}
         {...other}
       >
-        <TextField
-          fullWidth
-          label="Process Name"
-          {...getFieldProps('process_name')}
-          error={Boolean((touched.process_name || triedToClickPast) && errors.process_name)}
-          // helperText={touched.process_name && errors.process_name}
-          className={classes.margin}
-        />
-        <TextField
-          fullWidth
-          label="Level 2 - Process Name"
-          {...getFieldProps('process_L2_process_name')}
-          error={Boolean(touched.process_L2_process_name && errors.process_L2_process_name)}
-          helperText={'Level 2 process name or taxonomy reference'}
-          className={classes.margin}
-        />
-        <TextField
-          fullWidth
-          label="Level 3 - Process Name"
-          {...getFieldProps('process_L3_process_name')}
-          error={Boolean(touched.process_L3_process_name && errors.process_L3_process_name)}
-          helperText={'Level 3 process name or taxonomy reference'}
-          className={classes.margin}
-        />
-        <TextField
-          select
-          fullWidth
-          variant="outlined"
-          label="Pipeline"
-          {...getFieldProps('pipeline')}
-          // This select field needs formik.handleChange for formik to detect its value even though the other ones don't seem to need it for some reason
-          onChange={formik.handleChange}
-          // helperText="Please select your currency"
-          className={classes.margin}
-          error={Boolean((touched.pipeline || triedToClickPast) && errors.pipeline)}
-        >
-          {['Idea', 'Pipeline', 'Development', 'Production'].map(option => (
-            <MenuItem key={option} value={option}>
-              {option}
-            </MenuItem>
-          ))}
-        </TextField>
-
-        <TextField
-          fullWidth
-          multiline
-          minRows={3}
-          maxRows={5}
-          label="Process Overview"
-          {...getFieldProps('overview')}
-          error={Boolean(touched.overview && errors.overview)}
-          // helperText={touched.overview && errors.overview}
-          className={classes.margin}
-        />
-
-        <TextField
-          fullWidth
-          multiline
-          minRows={3}
-          maxRows={5}
-          label="Process Description"
-          {...getFieldProps('description')}
-          error={Boolean(touched.description && errors.description)}
-          // helperText={touched.description && errors.description}
-          className={classes.margin}
-        />
-
+        <Grid container spacing={3}>
+          <Grid item lg={12}>
+            <TextField
+              fullWidth
+              label="Process Name"
+              {...getFieldProps('process_name')}
+              error={Boolean((touched.process_name || triedToClickPast) && errors.process_name)}
+              // helperText={touched.process_name && errors.process_name}
+              className={classes.margin}
+            />
+          </Grid>
+          <Grid item lg={6}>
+            <TextField
+              fullWidth
+              label="Level 2 - Process Name"
+              {...getFieldProps('process_L2_process_name')}
+              error={Boolean(touched.process_L2_process_name && errors.process_L2_process_name)}
+              helperText={'Level 2 process name or taxonomy reference'}
+              className={classes.margin}
+            />
+          </Grid>
+          <Grid item lg={6}>
+            <TextField
+              fullWidth
+              label="Level 3 - Process Name"
+              {...getFieldProps('process_L3_process_name')}
+              error={Boolean(touched.process_L3_process_name && errors.process_L3_process_name)}
+              helperText={'Level 3 process name or taxonomy reference'}
+              className={classes.margin}
+            />
+          </Grid>
+          <Grid item lg={12}>
+            <TextField
+              select
+              fullWidth
+              variant="outlined"
+              label="Pipeline"
+              {...getFieldProps('pipeline')}
+              // This select field needs formik.handleChange for formik to detect its value even though the other ones don't seem to need it for some reason
+              onChange={formik.handleChange}
+              // helperText="Please select your currency"
+              className={classes.margin}
+              error={Boolean((touched.pipeline || triedToClickPast) && errors.pipeline)}
+            >
+              {['Idea', 'Pipeline', 'Development', 'Production'].map(option => (
+                <MenuItem key={option} value={option}>
+                  {option}
+                </MenuItem>
+              ))}
+            </TextField>
+          </Grid>
+          <Grid item lg={12}>
+            <TextField
+              fullWidth
+              multiline
+              minRows={3}
+              maxRows={5}
+              label="Process Overview"
+              {...getFieldProps('overview')}
+              error={Boolean(touched.overview && errors.overview)}
+              // helperText={touched.overview && errors.overview}
+              className={classes.margin}
+            />
+          </Grid>
+          <Grid item lg={12}>
+            <TextField
+              fullWidth
+              multiline
+              minRows={3}
+              maxRows={5}
+              label="Process Description"
+              {...getFieldProps('description')}
+              error={Boolean(touched.description && errors.description)}
+              // helperText={touched.description && errors.description}
+              className={classes.margin}
+            />
+          </Grid>
+        </Grid>
       </Form>
     </FormikProvider>
   );
--- src/views/custom/processes/NewProcessView/NewProcessFormOwnership.js
+++ src/views/custom/processes/NewProcessView/NewProcessFormOwnership.js
@@ -4,7 +4,8 @@ import PropTypes from 'prop-types';
 import { Form, FormikProvider } from 'formik';
 import { makeStyles } from '@material-ui/core/styles';
 import {
-    TextField
+    TextField,
+    Grid
 } from '@material-ui/core';
 
 // ----------------------------------------------------------------------
@@ -50,66 +51,75 @@ function NewProcessFormOwnership({ formik, onOpenPreview, className, ...other })
                 className={clsx(classes.root, className)}
                 {...other}
             >
-                <TextField
-                    fullWidth
-                    label="Project Sponsor"
-                    {...getFieldProps('sponsor')}
-                    error={Boolean(touched.sponsor && errors.sponsor)}
-                    helperText={'Who in the company is financing the project?'}
-                    className={classes.margin}
-                />
-
-                <TextField
-                    fullWidth
-                    label="Assigned Team"
-                    {...getFieldProps('team')}
-                    error={Boolean(touched.team && errors.team)}
-                    className={classes.margin}
-                />
-
-                <TextField
-                    fullWidth
-                    label="Process SME"
-                    {...getFieldProps('process_SME')}
-                    error={Boolean(touched.process_SME && errors.process_SME)}
-                    helperText={'Process subject matter expert name. The one with the most knowledge on the process.'}
-                    className={classes.margin}
-                />
-
-                <TextField
-                    fullWidth
-                    type='email'
-                    label="Process SME Email"
-                    {...getFieldProps('process_SME_email')}
-                    error={Boolean(touched.process_SME_email && errors.process_SME_email)}
-                    className={classes.margin}
-                />
-
-                <TextField
-                    fullWidth
-                    type='number'
-                    label="Process SME Phone Number"
-                    {...getFieldProps('process_SME_tel')}
-                    error={Boolean(touched.smePhone && errors.smePhone)}
-                    className={classes.margin}
-                />
-
-                <TextField
-                    fullWidth
-                    label="Process Owner Name"
-                    {...getFieldProps('owner_name')}
-                    error={Boolean(touched.ownerName && errors.ownerName)}
-                    className={classes.margin}
-                />
-
-                <TextField
-                    fullWidth
-                    label="Process Owner Email"
-                    {...getFieldProps('owner_email')}
-                    error={Boolean(touched.ownerEmail && errors.ownerEmail)}
-                    className={classes.margin}
-                />
-
+                <Grid container spacing={3}>
+                    <Grid item lg={12}>
+                        <TextField
+                            fullWidth
+                            label="Project Sponsor"
+                            {...getFieldProps('sponsor')}
+                            error={Boolean(touched.sponsor && errors.sponsor)}
+                            helperText={'Who in the company is financing the project?'}
+                            className={classes.margin}
+                        />
+                    </Grid>
+                    <Grid item lg={12}>
+                        <TextField
+                            fullWidth
+                            label="Assigned Team"
+                            {...getFieldProps('team')}
+                            error={Boolean(touched.team && errors.team)}
+                            className={classes.margin}
+                        />
+                    </Grid>
+                    <Grid item lg={4}>
+                        <TextField
+                            fullWidth
+                            label="Process SME"
+                            {...getFieldProps('process_SME')}
+                            error={Boolean(touched.process_SME && errors.process_SME)}
+                            helperText={'Process subject matter expert name. The one with the most knowledge on the process.'}
+                            className={classes.margin}
+                        />
+                    </Grid>
+                    <Grid item lg={4}>
+                        <TextField
+                            fullWidth
+                            type='email'
+                            label="Process SME Email"
+                            {...getFieldProps('process_SME_email')}
+                            error={Boolean(touched.process_SME_email && errors.process_SME_email)}
+                            className={classes.margin}
+                        />
+                    </Grid>
+                    <Grid item lg={4}>
+                        <TextField
+                            fullWidth
+                            type='number'
+                            label="Process SME Phone Number"
+                            {...getFieldProps('process_SME_tel')}
+                            error={Boolean(touched.smePhone && errors.smePhone)}
+                            className={classes.margin}
+                        />
+                    </Grid>
+                    <Grid item lg={6}>
+                        <TextField
+                            fullWidth
+                            label="Process Owner Name"
+                            {...getFieldProps('owner_name')}
+                            error={Boolean(touched.ownerName && errors.ownerName)}
+                            className={classes.margin}
+                        />
+                    </Grid>
+                    <Grid item lg={6}>
+                        <TextField
+                            fullWidth
+                            label="Process Owner Email"
+                            {...getFieldProps('owner_email')}
+                            error={Boolean(touched.ownerEmail && errors.ownerEmail)}
+                            className={classes.margin}
+                        />
+                    </Grid>
+                </Grid>
             </Form>
         </FormikProvider>
     );
